9acb5515c1beaad28582d695968dbe29e2d2d977
eyemnaughtreal <mykid8yours@gmail.com>
2024-11-22 15:15:07 -0500

    Initial Commit

---
+++ Brewly/BrewlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewlyApp.swift
+//  Brewly
+//
+//  Created by Mark Conley on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct BrewlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrewlyTests/BrewlyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BrewlyTests.swift
+//  BrewlyTests
+//
+//  Created by Mark Conley on 11/22/24.
+//
+
+import Testing
+@testable import Brewly
+
+struct BrewlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
